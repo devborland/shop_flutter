e32246d8ef1197ed0fc2830a443a5d7a75224feb
Dev Borland <devborland@gmail.com>
2021-02-22 12:22:21 +0300

    Making Orders Expandable

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -28,7 +28,7 @@ class CartPage extends StatelessWidget {
                   Spacer(),
                   Chip(
                     label: Text(
-                      '\$${cart.totalAmount}',
+                      '\$${cart.totalAmount.toStringAsFixed(2)}',
                       style: TextStyle(
                           color: Theme.of(context)
                               .primaryTextTheme
