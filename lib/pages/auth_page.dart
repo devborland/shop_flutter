8d2454c87c788e37c6cd9cb7b08020e7901a8da1
Dev Borland <devborland@gmail.com>
2021-03-08 10:54:42 +0300

    Using the  AnimatedBuilder widthet

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -124,9 +124,7 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.fastOutSlowIn,
       ),
     );
-    _heightAnimation.addListener(() => setState(() {
-          //...
-        }));
+    // _heightAnimation.addListener(() => setState(() {}));
   }
 
   dispose() {
@@ -221,13 +219,17 @@ class _AuthCardState extends State<AuthCard>
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      elevation: 4.0,
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, child) => Container(
+            // height: _authMode == AuthMode.Signup ? 320 : 260,
+            height: _heightAnimation.value.height,
+            constraints:
+                BoxConstraints(minHeight: _heightAnimation.value.height),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: child),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -293,6 +295,7 @@ class _AuthCardState extends State<AuthCard>
                     color: Theme.of(context).primaryColor,
                     textColor: Theme.of(context).primaryTextTheme.button.color,
                   ),
+                SizedBox(height: 10.0),
                 // ignore: deprecated_member_use
                 FlatButton(
                   child: Text(
